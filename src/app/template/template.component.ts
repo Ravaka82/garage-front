c809f8e7132ffef3c9b78a903bb3c8ca559897e7
Ravaka <rakotonarivosaholintsoaravaka@gmail.com>
2023-01-18 13:45:56 +0300

    listes type reparation
    listes type reparation

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TypeReparation } from '../Model/TypeReparation';
+import { TypeReparationService } from '../Service/type-reparation.service';
 
 @Component({
   selector: 'app-template',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent {
+  TypeReparation!: TypeReparation[];
+  constructor(private typeReparationservice: TypeReparationService,private router: Router) { }
 
+  ngOnInit(): void{
+    this.listRegion();
+  }
+  
+  listRegion(): void{//function liste
+    this.typeReparationservice.getAllTypeReparation()
+      .subscribe(
+        data => {
+          this.TypeReparation=data;
+        })
+   }
 }
