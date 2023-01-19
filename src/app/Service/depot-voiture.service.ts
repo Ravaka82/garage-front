9e31c3781a3bcc8591a06bed5189d0c53e751115
sarobidy <ratsaraefadahy@gmail.com>
2023-01-19 10:22:39 +0300

    deposer une voiture

---
+++ src/app/Service/depot-voiture.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Vehicule } from '../Model/vehicule';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepotVoitureService {
+
+  constructor(private http:HttpClient) { }
+  UrlDepot= 'http://localhost:8081/api/vehicule/createVehicule';
+
+  DepotVoiture(vehicule: Vehicule)
+  {
+    
+    return this.http.post<Vehicule>(this.UrlDepot,vehicule);
+  }
+}
