11141397dcdfb3575dfcb2e8172c31cf11d14bfa
Ravaka <rakotonarivosaholintsoaravaka@gmail.com>
2023-01-19 12:41:23 +0300

    redirection utilisateur
    redirection utilisateur

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TypeReparation } from '../Model/TypeReparation';
 import { TypeReparationService } from '../Service/type-reparation.service';
-
+import {MatSnackBar,MatSnackBarConfig} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -10,7 +10,7 @@ import { TypeReparationService } from '../Service/type-reparation.service';
 })
 export class TemplateComponent {
   TypeReparation!: TypeReparation[];
-  constructor(private typeReparationservice: TypeReparationService,private router: Router) { }
+  constructor(private _snackBar: MatSnackBar,private typeReparationservice: TypeReparationService,private router: Router) { }
 
   ngOnInit(): void{
     this.listRegion();
@@ -22,5 +22,20 @@ export class TemplateComponent {
         data => {
           this.TypeReparation=data;
         })
+   }
+   depotVoiture(){ //redirection le rehefa avy mpototra le icone reparation
+    if(!localStorage.getItem("idUser")){
+      this._snackBar.open("vous devriez d'abord vous connecter", 'Close',{
+        duration:2000,
+        // css matsnack bar dia any amn style.css ny css anreo
+        verticalPosition: 'top',
+        horizontalPosition: 'right',
+        panelClass: ['warning-alert']
+      });
+      this.router.navigate(['/login']);
+    }
+    else{
+      this.router.navigate(['/acceuil']);
+    }
    }
 }
