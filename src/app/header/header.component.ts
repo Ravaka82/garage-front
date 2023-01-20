5d39312c9752aa485302fc27a51c8962b4d3f898
sarobidy <ratsaraefadahy@gmail.com>
2023-01-20 18:57:13 +0300

    deconnexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Utilisateur } from '../Model/Utilisateur';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  Utilisateur: Utilisateur = new Utilisateur();
+  constructor(private router: Router){ }
+ngOnInit(): void {
 
+}
+deconnexion() {
+  localStorage.removeItem('idUser');
+  this.router.navigate(['Garage']);
+}
 }
