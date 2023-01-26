102546f79bda78d4c6deae8e2161b47474526ab5
Ravaka <rakotonarivosaholintsoaravaka@gmail.com>
2023-01-26 17:12:27 +0300

    send mail with upload pdf
    send mail with upload pdf

--- src/app/Service/paiement.service.ts
+++ src/app/Service/paiement.service.ts
@@ -12,6 +12,7 @@ export class PaiementService {
   url2='http://localhost:8080/api/paiement/findVehiculeEnAttente';
   Url3='http://localhost:8080/api/paiement/accepterPaiement';
   Url4='http://localhost:8080/api/paiement/getAllPaiementValider';
+  Url5='http://localhost:8080/api/paiement/sender';
 
   ValidationPaiement(vehicule: String, prix: number) {
     return this.http.post<Paiement>(this.UrlDepot, {vehicule, prix});
@@ -26,4 +27,13 @@ export class PaiementService {
   getListeVoitureValider(){
     return this.http.get<Paiement[]>(this.Url4);
   }
+  sendMail(to:string, subject:string, text:string, file: File, filename:string){
+    const formData = new FormData();
+    formData.append('to', to);
+    formData.append('subject', subject);
+    formData.append('text', text);
+    formData.append('file', file);
+    formData.append('filename', filename);
+    return this.http.post<Paiement>(this.Url5, formData);
+}
 }
--- src/app/envoye-mail/envoye-mail.component.ts
+++ src/app/envoye-mail/envoye-mail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { DateAdapter } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { PaiementService } from '../Service/paiement.service';
 @Component({
   selector: 'app-envoye-mail',
   templateUrl: './envoye-mail.component.html',
@@ -8,6 +11,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EnvoyeMailComponent implements OnInit {
   nomForm: FormGroup = {} as FormGroup;
+  submitting: boolean = false;
+  fileSource: any;
+  name!: '';
+  constructor(private paiementservice: PaiementService) {}
   ngOnInit(): void {
   this.initForm();
   }
@@ -18,4 +25,38 @@ export class EnvoyeMailComponent implements OnInit {
       text: new FormControl('', [Validators.required]),
       file: new FormControl('', [Validators.required]),
     })};
+    onFileChange(event:any) {
+      if (event.target.files.length > 0) {
+        const file = event.target.files[0];
+        this.name = event.target.files[0].name;
+        this.nomForm.patchValue({
+         file,
+        });
+      }
+    }
+    // onFileChange(event:any) {
+    //   if (event.target.files.length > 0) {
+    //     const file = event.target.files[0];
+    //     this.name = event.target.files[0].name;
+    //     this.nomForm.patchValue({
+    //       myFile: event.target.files[0]
+    //     })
+    //    console.log("file"+this.nomForm.get('<name>')?.setValue(file, {emitModelToViewChange: false}));
+    //   }
+    // }
+    onSubmit(){
+      const to = this.nomForm.get("to")?.value;
+      const subject = this.nomForm.get("subject")?.value;
+      const text = this.nomForm.get("text")?.value;
+      const file = this.nomForm.get("file")?.value;
+      const f = this.name;
+      const filename = f.split('.').slice(0, -1).join('.');
+      console.log("to"+to);
+      console.log("subject"+subject);
+      console.log("text"+text);
+      console.log("file"+file);
+      console.log("f"+f);
+      console.log("filename"+filename);
+      this.paiementservice.sendMail(to,subject,text,file,filename);
+    }
 }
