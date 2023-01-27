1e7e5c98c00639c2cde8dd37222b75ad0bc1d801
sarobidy <ratsaraefadahy@gmail.com>
2023-01-27 15:04:20 +0300

    update models

--- src/app/Model/Reparation.ts
+++ src/app/Model/Reparation.ts
@@ -3,7 +3,9 @@ import { Vehicule } from "./vehicule";
 
 export class Reparation{
     _id!: string;
-    avancement!: number;
+    dateHeureDebut!: Date;
+    dateHeureFin!: Date;
+    tempsReparation!: Date;
     statusUneReparation!: boolean;
     typeReparation!: TypeReparation;
     vehicule!:Vehicule;
