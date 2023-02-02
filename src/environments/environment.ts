47309026cc836a82e0be419e84f39d8fe112bf98
Ravaka <rakotonarivosaholintsoaravaka@gmail.com>
2023-02-02 16:55:33 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl:'https://m1p10mean-ravaka-sarobidy-frontend.vercel.app/api'
+    apiUrl:'https://m1p10mean-ravaka-sarobidy-backend.vercel.app/api'
 };
