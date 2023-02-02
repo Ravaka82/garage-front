5603a5c2b339bf72577f53ff6985ae44af134995
Ravaka <rakotonarivosaholintsoaravaka@gmail.com>
2023-02-02 09:47:22 +0300

    modif url
    modif url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl:'https://m1p10mean-ravaka-sarobidy-backend-cvr47odnd-ravaka82.vercel.app/api'
+    apiUrl:'https://m1p10mean-ravaka-sarobidy-backend.vercel.app/api'
 };
